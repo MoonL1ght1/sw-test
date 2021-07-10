9a48b05f8d8d7ee5af06eb93c99a506b7793c529
MoonL1ght1 <Ivanov_sergei111@mail.ru>
2021-07-10 18:44:24 +0500

    add Japa

---
+++ test/usersApiTest.spec.ts
@@ -0,0 +1,10 @@
+import test from 'japa'
+import { JSDOM } from 'jsdom'
+import supertest from 'supertest'
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+
+test.group('Tests:', () => {
+  test('ensure something', async (assert) => {
+  })
+})
